9bd83ab3be45b9b1236be0ccece7c0a169a4b282
Ali-Akber Saifee <ali@indydevs.org>
2018-01-14 20:23:54 +0800

    confligt version 0.1.0

---
+++ Formula/confligt.rb
@@ -0,0 +1,15 @@
+class Confligt < Formula
+  desc "Find conflicting branches"
+  homepage ""
+  url "https://github.com/alisaifee/confligt/releases/download/0.1.0/confligt_0.1.0_macOS_64-bit.tar.gz"
+  version "0.1.0"
+  sha256 "e7819d238860d6eb5a3847a7270a358e80743dda8a769a2a7ad57669c0d5d1ca"
+
+  def install
+    bin.install "confligt"
+  end
+
+  test do
+    
+  end
+end
