c850efb21d3baacceaf00055378cbfeb40222bd3
Ali-Akber Saifee <ali@indydevs.org>
2018-01-21 10:11:54 +0800

    confligt version 0.2.0

--- Formula/confligt.rb
+++ Formula/confligt.rb
@@ -1,9 +1,9 @@
 class Confligt < Formula
-  desc "Find conflicting branches"
+  desc "Find conflicting branches in git repositories"
   homepage ""
-  url "https://github.com/alisaifee/confligt/releases/download/0.1.0/confligt_0.1.0_macOS_64-bit.tar.gz"
-  version "0.1.0"
-  sha256 "e7819d238860d6eb5a3847a7270a358e80743dda8a769a2a7ad57669c0d5d1ca"
+  url "https://github.com/alisaifee/confligt/releases/download/0.2.0/confligt_0.2.0_macOS_64-bit.tar.gz"
+  version "0.2.0"
+  sha256 "8abd9b24477ed0d9ff6ee36d1e58abe6382d6732ac9f945d812bef53a1de4992"
 
   def install
     bin.install "confligt"
